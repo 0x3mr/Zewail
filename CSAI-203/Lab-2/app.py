3ae0c430af409d8332fa70a535dc6bc884498e5a
0x3mr <amrabdelfattah321@gmail.com>
2024-10-08 14:53:13 +0300

    lab practice

---
+++ CSAI-203/Lab-2/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    username = request.form.get('user')
+    coursename = request.form.get('coursename')
+    return redirect(url_for('index2', username=username, coursename=coursename))
+
+@app.route('/index2')
+def index2():
+    username = request.args.get('username')
+    coursename = request.args.get('coursename')
+    return render_template('index2.html', username=username, coursename=coursename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
