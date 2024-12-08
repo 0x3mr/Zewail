386b9cd17c8a0a30034ff09d576f7e895ceeb6ce
0x3mr <amrabdelfattah321@gmail.com>
2024-12-08 16:09:52 +0200

    fixed corrupt version

--- CSAI-201/Lab-9/src/MatrixGraph.h
+++ CSAI-201/Lab-9/src/MatrixGraph.h
@@ -3,38 +3,35 @@
 #ifndef MATRIX_GRAPH_
 #define MATRIX_GRAPH_
 
-
 #include <vector>
 #include "GraphADT.h"
 #include "Util.h"
 using namespace std;
 
-
 template<typename T>
 class MatrixGraph : public GraphADT<T>
 {
 private:
-	vector<T> vertices;
-	vector<vector<int>> adjMatrix;
+    vector<T> vertices;
+    vector<vector<int>> adjMatrix;
 
 public:
-	MatrixGraph();
+    MatrixGraph();
 
-	bool IsEmpty() const;
-	bool AddVertex(const T& v);
-	bool RemoveVertex(const T& v);
-	bool AddEdge(const T& fromV, const T& toV, int weight = 1);
-	void RemoveEdge(const T& fromV, const T& toV);
-	void DisplayNeighbors(const T& fromV) const;
-	bool EdgeExists(const T& fromV, const T& toV) const;
-	bool IsAdjacent(const T& v1, const T& v2) const;
-	void PrintGraph();
-	void DFS(const T& startV) const;
-	void BFS(const T& startV) const;
-	void TopologicalSort() const;
+    bool IsEmpty() const;
+    bool AddVertex(const T& v);
+    bool RemoveVertex(const T& v);
+    bool AddEdge(const T& fromV, const T& toV, int weight = 1);
+    void RemoveEdge(const T& fromV, const T& toV);
+    void DisplayNeighbors(const T& fromV) const;
+    bool EdgeExists(const T& fromV, const T& toV) const;
+    bool IsAdjacent(const T& v1, const T& v2) const;
+    void PrintGraph();
+    void DFS(const T& startV) const;
+    void BFS(const T& startV) const;
+    void TopologicalSort() const;
 
-
-	~MatrixGraph();
+    ~MatrixGraph();
 };
 
 #endif
