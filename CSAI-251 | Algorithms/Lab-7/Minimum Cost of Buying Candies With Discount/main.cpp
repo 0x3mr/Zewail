d905ae7ba953c5c5cd59f39c0259590593d283b9
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2025-04-29 00:37:55 +0300

    Solved

--- CSAI-251 | Algorithms/Lab-7/Minimum Cost of Buying Candies With Discount/main.cpp
+++ CSAI-251 | Algorithms/Lab-7/Minimum Cost of Buying Candies With Discount/main.cpp
@@ -1,1 +1,30 @@
-// coming soon
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n, o, cost = 0;
+    std::cin >> n;
+    std::vector<int> amr;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> o;
+        amr.push_back(o);
+    }
+    std::sort(amr.begin(), amr.end());
+    while (amr.size() > 2)
+    {
+        for (int i = 2; i >= 0; i--)
+        {
+            if (i) cost += amr.back();
+            amr.pop_back();
+        }
+    }
+    while (amr.size())
+    {
+        cost += amr.back();
+        amr.pop_back();
+    }
+    std::cout << cost << std::endl;
+    return 0;
+}
